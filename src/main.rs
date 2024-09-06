99a77368f5f072248c82c957d9f30fbd0f4baaf4
George MacKerron <george@mackerron.co.uk>
2024-09-06 12:35:57 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use fastembed::{TextEmbedding, TokenizerFiles, UserDefinedEmbeddingModel};
+
+macro_rules! local_tokenizer_files {
+    // NOTE: macro assumes /unix/style/paths
+    ($folder:literal) => {
+        TokenizerFiles {
+            tokenizer_file: include_bytes!(concat!($folder, "/tokenizer.json")).to_vec(),
+            config_file: include_bytes!(concat!($folder, "/config.json")).to_vec(),
+            special_tokens_map_file: include_bytes!(concat!($folder, "/special_tokens_map.json"))
+                .to_vec(),
+            tokenizer_config_file: include_bytes!(concat!($folder, "/tokenizer_config.json"))
+                .to_vec(),
+        }
+    };
+}
+
+macro_rules! local_model {
+    // NOTE: macro assumes /unix/style/paths
+    ($model:ident, $folder:literal) => {
+        $model {
+            onnx_file: include_bytes!(concat!($folder, "/model.onnx")).to_vec(),
+            tokenizer_files: local_tokenizer_files!($folder),
+        }
+    };
+}
+
+fn main() {
+    let model_files = local_model!(UserDefinedEmbeddingModel, "../bge_small_en_v15");
+
+    let model = TextEmbedding::try_new_from_user_defined(model_files, Default::default())
+        .expect("Couldn't load model bge_small_en_v15");
+
+    let result = model
+        .embed(vec!["The quick brown fox jumps over the lazy dog"], None)
+        .expect("Unable to generate bge_small_en_v15 embeddings");
+
+    println!("{:?}", result);
+}
